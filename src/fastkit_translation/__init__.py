48ced9498e12475cf11ed43dd89e5d5d34584768
radomirbrkovic <radomir.brkovic@pmf.edu.rs>
2026-07-04 09:41:57 +0200

    Export helper methods

---
+++ src/fastkit_translation/__init__.py
@@ -0,0 +1,8 @@
+from .locale import set_locale, set_default_locale, get_default_locale, get_locale
+
+__all__ = [
+    'set_locale',
+    'set_default_locale',
+    'get_default_locale',
+    'get_locale'
+]
